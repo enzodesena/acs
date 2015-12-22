d912827cbb3145dedc47db5464f4919935074e4f
Enzo De Sena <enzodesena@me.com>
2015-12-22 19:17:59 +0100

    Corrected bug in VMUM MLE

--- circ_vmum_est_ml.m
+++ circ_vmum_est_ml.m
@@ -1,11 +1,31 @@
-function [mu_hat, k_hat, p1_hat, p2_hat, p3_hat, ll] = circ_vmum_est_ml(data, x0)
+function [mu_hat, k_hat, p1_hat, p2_hat, p3_hat, ll] = circ_vmum_est_ml(data, mu_0, k_0, p1_0, p2_0, p3_0)
 options = optimoptions('fmincon', 'Display', 'off');
+
+
+%% Asserts
+assert(p1_0>=0.0 & p1_0<=1.0);
+assert(p2_0>=0.0 & p2_0<=1.0);
+assert(p3_0>=0.0 & p3_0<=1.0);
+assert(abs((p1_0+p2_0+p3_0)-1.0)<1E-10);
+assert(isscalar(mu_0) & isscalar(k_0));
+assert(isscalar(p1_0) & isscalar(p2_0) & isscalar(p3_0));
+assert(isvector(data));
+
+%% Adjust initial point to problem domain
+if k_0 < 0
+    k_0 = -k_0;
+    mu_0 = mu_0 + pi;
+    temp = p1_0;
+    p1_0 = p2_0;
+    p2_0 = temp;
+end
+mu_0 = mod(mu_0, 2*pi);
 
 %% Run
 params = fmincon(@(params) ...
            -getll(params, data), ...
-           x0, [], [], [0, 0, 1, 1, 1], 1, ...
-           [-pi, 0, 0, 0, 0], [pi, inf, 1, 1, 1], [], options);
+           [mu_0, k_0, p1_0, p2_0, p3_0], [], [], [0, 0, 1, 1, 1], 1, ...
+           [0, 0, 0, 0, 0], [2*pi, inf, 1, 1, 1], [], options);
        
 %% Assign output values
 ll = getll(params, data);
