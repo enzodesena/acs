eb8c7b86c117cf658e50075d0bfb58c69c129647
Enzo De Sena <enzodesena@me.com>
2016-01-12 01:11:01 +0100

    Printout in vMUM asserts to understand what's going on.

--- lib/circ_vmum_asserts.m
+++ lib/circ_vmum_asserts.m
@@ -13,4 +13,10 @@ assert(isscalar(p1) & isscalar(p2) & isscalar(p3));
 assert(p1>-tol & p1<1.0+tol);
 assert(p2>-tol & p2<1.0+tol);
 assert(p3>-tol & p3<1.0+tol);
-assert(abs((p1+p2+p3)-1.0)<tol);
+if abs((p1+p2+p3)-1.0)>tol
+    display([p1, p2, p3])
+    assert(false);
+    %assert(abs((p1+p2+p3)-1.0)<tol);
+end
+
+   
--- test/run_tests.m
+++ test/run_tests.m
@@ -1,4 +1,4 @@
-
+function run_tests
 
 %% Testing drnd(values, pmf, N)
 N = 100000;
@@ -7,6 +7,25 @@ pmf = [0.1, 0.3, 0.6];
 obs = drnd(values, pmf, N);
 for i=1:length(values)
     epmf(i) = sum(obs==values(i))/N;
+end
+assert(is_equal(epmf, pmf, 0.01));
+
+%% Testing vMUM standard
+[mu, k, p1, p2, p3] = circ_vmum_standard(pi, 10, 0.3, 0.5, 0.2);
+assert(is_equal(circ_vmum_pdf(mu, k, p1, p2, p3, linspace(0,2*pi,100)),...
+                circ_vmum_pdf(pi, 10, 0.3, 0.5, 0.2, linspace(0,2*pi,100))));
+            
+[mu, k, p1, p2, p3] = circ_vmum_standard(pi, -10, 0.3, 0.5, 0.2);
+assert(is_equal(circ_vmum_pdf(mu, k, p1, p2, p3, linspace(0,2*pi,100)),...
+                circ_vmum_pdf(pi, -10, 0.3, 0.5, 0.2, linspace(0,2*pi,100))));
+
+display('All tests passed!');
+
+end
+
+function result = is_equal(a, b, tol)
+if nargin == 2
+    tol = 1e-10;
 end
 
 assert(all(abs(epmf-pmf) < 0.01));
