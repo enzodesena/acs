3e5ef92e47aba1c74ed09b19ea459c32b41b3c15
Enzo De Sena <enzodesena@me.com>
2016-03-14 11:27:35 +0000

    Changed from mu to mi in vmum_test_ss to avoid problems with built-in function mu.

--- circ_vmum_test_ss.m
+++ circ_vmum_test_ss.m
@@ -1,12 +1,12 @@
-function [H, P, llrt, exitflag] = circ_vmum_test_ss(data, mu, alpha, options)
+function [H, P, llrt, exitflag] = circ_vmum_test_ss(data, mi, alpha, options)
 %CIRC_VMUM_TEST_SS one sample test for vMUM model
 %
 %   Audio Circular Statistics (ACS) library
 %   Copyright 2016 Enzo De Sena
 
 
-if nargin <= 1 || isempty(mu)
-    mu = 0;
+if nargin <= 1 || isempty(mi)
+    mi = 0;
 end
 
 if nargin <= 2 || isempty(alpha)
@@ -22,17 +22,17 @@ end
 
 %% Assert
 assert(iscolumn(data));
-assert(isscalar(mu));
+assert(isscalar(mi));
 assert(isscalar(alpha));
 
 %% Calculate ll of null hypothesis
 % Calculate starting point using vMUM-MM method, assuming mu=mu_0
-[mu_mm_0, k_mm_0, p1_mm_0, p2_mm_0, p3_mm_0] = circ_vmum_est_mm(data, mu);
-assert(abs(mu_mm_0-mu)<1e-10);
+[mu_mm_0, k_mm_0, p1_mm_0, p2_mm_0, p3_mm_0] = circ_vmum_est_mm(data, mi);
+assert(abs(mu_mm_0-mi)<1e-10);
 
 % Calculate ML of parameters, assuming mu=mu_0
 [params_0, ll_0_neg, exitflag_0] = fmincon(@(params) ...
-                   -circ_vmum_ll(mu, params(1), params(2), ...
+                   -circ_vmum_ll(mi, params(1), params(2), ...
                                  params(3), params(4), data, true), ...
                    [k_mm_0, p1_mm_0, p2_mm_0, p3_mm_0], [], [], ...
                    [0, 1, 1, 1], 1, ...
@@ -47,7 +47,7 @@ ll_0 = -ll_0_neg;
 % since it often happens that algorithm gets stuck 
 % in a local minimum near the solution with mu=mu_0.
 [~, ~, ~, ~, ~, ll_1_a, exitflag_1_a, ~] = ...
-    circ_vmum_est_ml(data, mu, params_0(1), ...
+    circ_vmum_est_ml(data, mi, params_0(1), ...
                      params_0(2), params_0(3), params_0(4), options);
                  
 % Calculate ML estimate of mu_hat, k_hat and p, using the vMUM-ML
