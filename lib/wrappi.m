067a5f2eeadc107be385f2c6a2e5acb03a330d1f
Enzo De Sena <enzodesena@me.com>
2016-03-21 20:53:36 +0000

    Fixed wrappi.

--- lib/wrappi.m
+++ lib/wrappi.m
@@ -1,5 +1,6 @@
-
 % wraps to -pi pi
-function wrapped_angle = wrapToMinusPiPi(angle)
-wrapped_angle = mod(angle+pi,2*pi)-pi;
+function wrapped_angle = wrappi(angle)
+wrapped_angle = rem(angle, 2*pi);
+above_pi = abs(wrapped_angle) > pi;
+wrapped_angle(above_pi) = wrapped_angle(above_pi) - 2*pi*sign(wrapped_angle(above_pi));
 end
